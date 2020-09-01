f964f4fae44797cefbe9f487c53f89ba84baa1ea
KonghaYao <20192831006@m.scnu.edu.cn>
2020-09-01 15:27:41 +0800

    2020/9/1

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mode: "development", //production默认会压缩代码，development则不会。
-    entry: __dirname + "/JSpider-all.js", //入口文件，多个入口可以对象kv方式传入
-    output: {
-        path: __dirname + "/build",
-        filename: "JSpider.js",
-    },
-};
