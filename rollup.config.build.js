beb44878234077155cedb14c3237d6f562163c03
KonghaYao <20192831006@m.scnu.edu.cn>
2021-06-30 10:12:10 +0800

    change: 更新 rollup 插件版本

--- rollup.config.build.js
+++ rollup.config.build.js
@@ -1,63 +1,63 @@
-import resolve from "rollup-plugin-node-resolve"; // 帮助寻找node_modules里的包
-import commonjs from "rollup-plugin-commonjs"; // 将非ES6语法的包转为ES6可用
-import { terser } from "rollup-plugin-terser";
-import license from "rollup-plugin-license";
-import del from "rollup-plugin-delete";
-import json from "@rollup/plugin-json";
+import resolve from '@rollup/plugin-node-resolve'; // 帮助寻找node_modules里的包
+import commonjs from '@rollup/plugin-commonjs'; // 将非ES6语法的包转为ES6可用
+import { terser } from 'rollup-plugin-terser';
+import license from 'rollup-plugin-license';
+import del from 'rollup-plugin-delete';
+import json from '@rollup/plugin-json';
 const plugins = [
     json(),
     resolve({
-        browser: true
+        browser: true,
     }),
     commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
     terser(),
     license({
         banner: {
             content: {
-                file: "./LICENSE"
-            }
-        }
-    })
+                file: './LICENSE',
+            },
+        },
+    }),
 ];
 export default [
     {
-        input: "src/JSpider.js", // 通用版本打包
-        plugins: [del({ targets: "dist/*" }), ...plugins],
+        input: 'src/JSpider.js', // 通用版本打包
+        plugins: [del({ targets: 'dist/*' }), ...plugins],
         output: [
             {
                 // 打包出口
-                file: "./dist/JSpider.min.js",
-                format: "iife",
-                name: "JSpider"
+                file: './dist/JSpider.min.js',
+                format: 'iife',
+                name: 'JSpider',
             },
             {
                 // 打包出口
-                file: "./dist/JSpider.umd.min.js",
-                format: "umd",
-                name: "JSpider"
+                file: './dist/JSpider.umd.min.js',
+                format: 'umd',
+                name: 'JSpider',
             },
             {
                 // 打包出口
-                file: "./dist/JSpider.cjs.min.js",
-                format: "cjs",
-                exports: "default"
+                file: './dist/JSpider.cjs.min.js',
+                format: 'cjs',
+                exports: 'default',
             },
             {
                 // 打包出口
-                file: "./dist/JSpider.esm.min.js",
-                format: "es"
-            }
-        ]
+                file: './dist/JSpider.esm.min.js',
+                format: 'es',
+            },
+        ],
     },
     {
-        input: "src/JSpider.esm.js", // 这是一个可以进行 TreeShake 的版本
+        input: 'src/JSpider.esm.js', // 这是一个可以进行 TreeShake 的版本
         plugins,
         output: [
             {
                 // 打包出口
-                file: "./dist/JSpider.js",
-                format: "es"
-            }
-        ]
-    }
+                file: './dist/JSpider.js',
+                format: 'es',
+            },
+        ],
+    },
 ];
--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,27 +1,27 @@
-import resolve from "rollup-plugin-node-resolve"; // 帮助寻找node_modules里的包
-import commonjs from "rollup-plugin-commonjs"; // 将非ES6语法的包转为ES6可用
-import json from "@rollup/plugin-json";
+import resolve from '@rollup/plugin-node-resolve'; // 帮助寻找node_modules里的包
+import commonjs from '@rollup/plugin-commonjs'; // 将非ES6语法的包转为ES6可用
+import json from '@rollup/plugin-json';
 
 export default {
-    input: "src/JSpider.js", // 打包入口
+    input: 'src/JSpider.js', // 打包入口
     output: [
         {
             // 打包出口
-            file: "./dist/JSpider.esm.min.js",
-            format: "es"
+            file: './dist/JSpider.esm.min.js',
+            format: 'es',
         },
         {
             // 打包出口
-            file: "./dist/JSpider.min.js",
-            format: "iife",
-            name: "JSpider"
-        }
+            file: './dist/JSpider.min.js',
+            format: 'iife',
+            name: 'JSpider',
+        },
     ],
     plugins: [
         json(),
         resolve({
-            browser: true
+            browser: true,
         }),
-        commonjs() // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
-    ]
+        commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
+    ],
 };
