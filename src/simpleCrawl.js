ec8867269826b56392cd0daeb81b84dda03616c7
KonghaYao <20192831006@m.scnu.edu.cn>
2021-07-02 21:06:33 +0800

    fix: 修复simpleCrawl 对二进制数据下载不当的情况

--- src/simpleCrawl.js
+++ src/simpleCrawl.js
@@ -2,13 +2,13 @@
  * @Author: KonghaYao
  * @Date: 2021-06-28 21:05:51
  * @Last Modified by: KonghaYao
- * @Last Modified time: 2021-06-29 16:25:48
+ * @Last Modified time: 2021-07-02 08:53:37
  */
 import { JSpider } from './core/index.js';
 import { Request, Download } from './plugins/index.js';
 
 export function simpleCrawl(urls) {
-    const spider = new JSpider(Request(), Download());
+    const spider = new JSpider(Request({ returnType: 'blob' }), Download());
     spider.apply(urls);
     return spider;
 }
